7b38d4568a595af447e8343e38173c2b86d0f5b9
Frederick Cabedoce <frederick.cabedoce@hotmail.com>
2023-04-15 16:36:01 -0400

    Spring initializr commit

---
+++ src/main/java/cabedoce/cloud/solutions/userjava17/UserJava17Application.java
@@ -0,0 +1,13 @@
+package cabedoce.cloud.solutions.userjava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserJava17Application.class, args);
+	}
+
+}
---
+++ src/test/java/cabedoce/cloud/solutions/userjava17/UserJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package cabedoce.cloud.solutions.userjava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
